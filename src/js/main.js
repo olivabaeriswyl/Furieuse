e37f249ace92fa21133fe6efaae4c6b535c9c2c5
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2024-11-12 17:14:28 +0100

    export et rajout des images sur le html

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".slider-container", {
+//   x: "-75%",
+//   scrollTrigger: {
+//     trigger: ".slider-mask",
+//     start: "top top",
+//     scrub: 1,
+//     pin: true,
+//   },
+// });
